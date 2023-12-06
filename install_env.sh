8eca9a09a801fa7376c61397a401bf65ac067190
Sumeet Batra <sbatra@nvidia.com>
2023-12-06 10:35:11 -0800

    new conda env yaml file and install script

---
+++ install_env.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+conda env create -f environment.yml
+
+# for CUDA 11 and cuDNN 8.2 or newer
+wget https://storage.googleapis.com/jax-releases/cuda11/jaxlib-0.3.25+cuda11.cudnn82-cp39-cp39-manylinux2014_x86_64.whl
+pip install jaxlib-0.3.25+cuda11.cudnn82-cp39-cp39-manylinux2014_x86_64.whl
+
+# for CUDA 11 and cuDNN 8.0.5 or newer, uncomment this and comment out the previous two lines
+#wget https://storage.googleapis.com/jax-releases/cuda11/jaxlib-0.3.25+cuda11.cudnn805-cp39-cp39-manylinux2014_x86_64.whl
+#pip install jaxlib-0.3.25+cuda11.cudnn805-cp39-cp39-manylinux2014_x86_64.whl
+
+pip install jax==0.3.25
+pip install jaxopt==0.5.5
+
+rm jaxlib-0.3.25+cuda11.cudnn82-cp39-cp39-manylinux2014_x86_64.whl
